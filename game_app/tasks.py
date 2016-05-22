2bafe2456dbce94f498ff3e731b58275221e76a0
Zoe Jin <zoe.jin@britny.se>
2016-05-22 13:36:52 +0200

    deal with giant bomb api search result multi-pages

--- game_app/tasks.py
+++ game_app/tasks.py
@@ -9,7 +9,7 @@ from game_app.models import Game
 logger = logging.getLogger(__name__)
 
 
-def fetch_games_by_keywords(keyword, resource_type):
+def fetch_games_by_keywords(keyword, resource_type, page=1):
     headers = {'User-Agent':
                    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
@@ -17,7 +17,8 @@ def fetch_games_by_keywords(keyword, resource_type):
                             params={'api_key': settings.GIANTBOMB_PUBLIC_API_KEY,
                                     'format': settings.FORMAT,
                                     'query': keyword,
-                                    'resources': resource_type},
+                                    'resources': resource_type,
+                                    'page': page},
                             headers=headers)
 
     if json.loads(response.text).get('status_code') != 1:
@@ -27,3 +28,8 @@ def fetch_games_by_keywords(keyword, resource_type):
             game, created = Game.objects.get_or_create(
                 game_id=str(g.get('id')))
             game.update_game(g)
+
+            if json.loads(response.text).get('number_of_page_results') > 0:
+                page += 1
+                fetch_games_by_keywords(keyword, resource_type, page)
+
